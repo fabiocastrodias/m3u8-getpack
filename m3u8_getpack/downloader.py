bb4aba71075dd3f5893fb54465eb229d621dc73a
Fabio Dias <dias.c.fabio@gmail.com>
2025-04-27 23:48:33 -0300

    fix(feat): improve download reliability by resolving stuck downloads and enhancing thread handling to avoid missing files

--- m3u8_getpack/downloader.py
+++ m3u8_getpack/downloader.py
@@ -8,14 +8,26 @@ from concurrent.futures import ThreadPoolExecutor
 class Downloader:
     def __init__(
                 self, urls, ext, total_segments,
-                temp_path=".tmp", match=r"\/video(\d+)\.ts$",
-                max_threads=32):
+                max_threads, temp_path=".tmp",
+                match=r"\/video(\d+)\.ts$"):
         self.urls = urls
         self.ext = ext
         self.total_segments = total_segments
         self.temp_path = temp_path
         self.match = match
+        if max_threads is None:
+            max_threads = 128
         self.max_threads = os.cpu_count() * max_threads
+
+    def download(self, url, output):
+        retries = 0
+        while True:
+            try:
+                wget.download(url, output, bar=None)
+                break
+            except Exception:
+                if retries > 500:
+                    break
 
     def download_worker(self):
         while self.urls.qsize() > 0:
@@ -28,7 +40,7 @@ class Downloader:
                 output = os.path.join(
                     self.temp_path,
                     f"{current_num}{self.ext}")
-                wget.download(url, output, bar=None)
+                self.download(url, output)
             finally:
                 self.urls.task_done()
 
